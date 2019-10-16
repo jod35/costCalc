f1dee78433538fa56bf51cc7a7a4ebfb13a0e667
jod35 <jodestrevin@gmail.com>
2019-10-16 18:37:34 +0300

    made the logic better

--- cost/routes.py
+++ cost/routes.py
@@ -9,13 +9,11 @@ def calc():
    cost_price=form.cost_price.data
    mark_up=form.mark_up.data
    discount=form.discount.data
-   
-   profit=((mark_up/100) * cost_price)
-
-   selling_price = ((cost_price + profit) - (cost_price * (discount/100)))
-
+   profit=(cost_price*(mark_up/100))
+   s_price=cost_price + profit
+   disc=(discount/100) * s_price 
+   selling_price=s_price -disc
    new_calc=Calculation(cost_price=cost_price,discount=discount,mark_up=mark_up,selling_price=selling_price)
-
    db.session.add(new_calc)
    db.session.commit()
    flash("The new selling price is UGX%s"%selling_price)
@@ -28,4 +26,9 @@ def index():
    return render_template("index.html",form=form,calculations=calculations)
 
 
-   
+@app.route('/delete/<int:id>')
+def delete(id):
+   calculation_to_be_deleted=Calculation.query.get_or_404(id)
+   db.session.delete(calculation_to_be_deleted)
+   db.session.commit()
+   return redirect('index')
